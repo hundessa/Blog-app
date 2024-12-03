0c9c127c4f1ff83bb045c0e121ab0978b87017ab
hundessa <hundserb@gmail.com>
2024-12-03 15:34:38 -0800

    finish the featured posts section

--- resources/views/components/posts-section.blade.php
+++ resources/views/components/posts-section.blade.php
@@ -1,9 +1,57 @@
 <div class="pt-24 post-section">
     <div class="">
-        <div class="flex justify-center">
-            <div>
-                <h1 class="text-3xl oxygen mb-8">Featured Posts</h1>
-                <hr class="h-4 w-60 text-gray-400 flex justify-center ml-[-10px]" />
+        <div class="text-center">
+            <h1 class="text-3xl oxygen">Featured Posts</h1>
+            <hr class="h-4 w-60 text-gray-400 mx-auto my-4" />
+        </div>
+        <div class="flex justify-center space-x-6">
+            <div class="relative overflow-hidden rounded-lg transform hover:-translate-y-2 transition duration-300 group">
+                <div class="post-section-bg1 transition-transform duration-500 ease-in-out group-hover:scale-110"></div>
+                <a href="">
+                    <div class="post-sectionbg1"></div>
+                    <div class="absolute top-5 left-6">
+                        <h1 class="text-white bg-purple-400 py-2 px-4 rounded-md">PHOTOGRAPHY</h1>
+                    </div>
+                    <div class="absolute bottom-4 left-8 space-y-6">
+                        <div class="space-y-2">
+                            <h1 class="text-white text-4xl">The Road Ahead</h1>
+                            <p class="text-gray-400 relative left-1 text-xl">The road ahead might be paved - it might not be.</p>
+                        </div>
+                        <div class="flex">
+                            <div class="text-white flex">
+                                <img src="{{ asset('images/bg.jpg') }}" alt="user profile picture" class="w-10 h-10 rounded-full">
+                                <h1 class="flex items-center my-auto ml-4 text-gray-300">Mat Demins</h1>
+                            </div>
+                            <div class="text-gray-400 flex items-center my-auto justify-end ml-auto mr-14">
+                                September 12,2024
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="transform hover:-translate-y-2 transition duration-300">
+                <a href="">
+                    <div class="relative rounded-lg overflow-hidden">
+                        <div class="post-section-bg2"></div>
+                        <div class="absolute top-5 left-6">
+                            <h1 class="text-white bg-amber-500 py-2 px-4 rounded-md">ADVENTURE</h1>
+                        </div>
+                        <div class="absolute bottom-4 left-8 space-y-6">
+                            <div class="space-y-2">
+                                <h1 class="text-white text-4xl">The Road Ahead</h1>
+                                <p class="text-gray-400 relative left-1 text-xl">The road ahead might be paved - it might not be.</p>
+                            </div>
+                            <div class="flex">
+                                <div class="text-white flex"> <img src="{{ asset('images/bg.jpg')}}" alt="user profile picture" class="w-10 h-10 rounded-full">
+                                    <h1 class="flex items-center my-auto ml-4 text-gray-300">Mat Demins</h1>
+                                </div>
+                                <div class="text-gray-400 flex items-center my-auto justify-end ml-auto mr-14">
+                                    September 12,2024
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
